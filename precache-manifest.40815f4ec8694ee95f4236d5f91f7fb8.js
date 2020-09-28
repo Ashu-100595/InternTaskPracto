f209b817cd7d1494488e2e677855a5a4ba6193f5
Ashu-100595 <pashutosh100595@gmail.com>
2020-09-28 12:19:36 +0530

    Updates

---
+++ precache-manifest.40815f4ec8694ee95f4236d5f91f7fb8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eaadd90d77d3264e9985ee34d67ed93",
+    "url": "/InternTaskPracto/index.html"
+  },
+  {
+    "revision": "f9a5274c8b45678f481f",
+    "url": "/InternTaskPracto/static/css/2.444f498c.chunk.css"
+  },
+  {
+    "revision": "6d57fe1b906dc237e2e9",
+    "url": "/InternTaskPracto/static/css/main.f9187319.chunk.css"
+  },
+  {
+    "revision": "f9a5274c8b45678f481f",
+    "url": "/InternTaskPracto/static/js/2.ea0d4601.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/InternTaskPracto/static/js/2.ea0d4601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d57fe1b906dc237e2e9",
+    "url": "/InternTaskPracto/static/js/main.ade67472.chunk.js"
+  },
+  {
+    "revision": "c73aa8130af36a8281a0",
+    "url": "/InternTaskPracto/static/js/runtime-main.c890801f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/InternTaskPracto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/InternTaskPracto/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/InternTaskPracto/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/InternTaskPracto/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/InternTaskPracto/static/media/slick.f97e3bbf.svg"
+  }
+]);
